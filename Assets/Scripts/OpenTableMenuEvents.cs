b633385bc54139c663b861d1699d23312f24783a
BuildTools <unconfigured@null.spigotmc.org>
2026-01-25 16:11:15 -0800

    opentable and premadegame scene/menus added

---
+++ Assets/Scripts/OpenTableMenuEvents.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class OpenTableMenuEvents : MonoBehaviour {
+    private UIDocument document;
+
+    private Button singleplayerButton;
+    private Button multiplayerButton;
+    private Button backButton;
+
+    private void Awake() {
+        document = GetComponent<UIDocument>();
+
+        singleplayerButton = document.rootVisualElement.Q("Singleplayer") as Button;
+        singleplayerButton.RegisterCallback<ClickEvent>(OnSingleplayerClicked);
+        multiplayerButton = document.rootVisualElement.Q("Multiplayer") as Button;
+        multiplayerButton.RegisterCallback<ClickEvent>(OnMultiplayerGameClicked);
+        backButton = document.rootVisualElement.Q("Back") as Button;
+        backButton.RegisterCallback<ClickEvent>(OnBackClicked);
+    }
+
+    private void OnSingleplayerClicked(ClickEvent e) {
+        Debug.Log("single clicked");
+    }
+
+    private void OnMultiplayerGameClicked(ClickEvent e) {
+        Debug.Log("multi clicked");
+    }
+
+    private void OnBackClicked(ClickEvent e) {
+        Debug.Log("back clicked");
+    }
+}
---
+++ Assets/Scripts/PremadeGameMenuEvents.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PremadeGameMenuEvents : MonoBehaviour {
+    private UIDocument document;
+
+    private Button cribbageButton;
+    private Button texasHoldEmButton;
+    private Button goFishButton;
+    private Button backButton;
+
+    private void Awake() {
+        document = GetComponent<UIDocument>();
+
+        cribbageButton = document.rootVisualElement.Q("Cribbage") as Button;
+        cribbageButton.RegisterCallback<ClickEvent>(OnCribbageClicked);
+        texasHoldEmButton = document.rootVisualElement.Q("TexasHoldEm") as Button;
+        texasHoldEmButton.RegisterCallback<ClickEvent>(OnTexasHoldEmClicked);
+        goFishButton = document.rootVisualElement.Q("GoFish") as Button;
+        goFishButton.RegisterCallback<ClickEvent>(OnGoFishClicked);
+        backButton = document.rootVisualElement.Q("Back") as Button;
+        backButton.RegisterCallback<ClickEvent>(OnBackClicked);
+    }
+
+    private void OnCribbageClicked(ClickEvent e) {
+        Debug.Log("cribbage clicked");
+    }
+
+    private void OnTexasHoldEmClicked(ClickEvent e) {
+        Debug.Log("texas clicked");
+    }
+
+    private void OnGoFishClicked(ClickEvent e) {
+        Debug.Log("go fish clicked");
+    }
+
+    private void OnBackClicked(ClickEvent e) {
+        Debug.Log("back clicked");
+    }
+}
